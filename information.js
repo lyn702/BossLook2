f16ce958f9f11a6ea25da9a07a4dafc8c341ffc1
towsu <1029187668@qq.com>
2017-12-20 18:59:50 +0800

    Liyn
    20号，BossLook第二版 新增API

--- information.js
+++ information.js
@@ -50,6 +50,7 @@ let today_xinxi = function () {
                     </div>
                     `
                     su.html = su.html + t
+                    // log(su.html)
                     if (i === infos.length - 1) {
                         $('.xm').html(su.html)
                     }
@@ -80,20 +81,52 @@ let today_shouru = function () {
         success: function(res) {
             log(res)
             // su.html = ''
-            let money = res.info.today_income
-            let people = res.info.today_visitors
-            let order = res.info.today_orders
-            log(money, people, order)
-            let t = `
-                <div class="number-1">${order}</div>
-                <div class="number-2">人数</div>
-            `
-            su.html =  t
-            $('.secondline-all').html(su.html)
+            if (res.result === 0) {
+                let money = res.info.today_income
+                let people = res.info.today_visitors
+                let order = res.info.today_orders
+                let todayzj = res.info.today_count
+                // log(su['order'])
+                log(money, people, order)
+                let t = `
+                    <div class="number-1">${order}</div>
+                    <div class="number-2">${todayzj}</div>
+                `
+                su.html =  t
+                $('.secondline-all').html(su.html)
+            }
         }
     })
     $.ajax(request)
 }
+
+// 新增人数显示（今日数据）
+// let people_count = function () {
+//     let request = ({
+//         url: "http://120.79.12.95/newapi/Wxbossboard/newIncreaseMemberCount",
+//         data: {
+//             // "action": 'incomeAndvisitor',
+//             "scene_id": su['scene_id'],
+//         },
+//         header: {
+//             "Content-Type": "application/json"
+//         },
+//         method: 'POST',
+//         success: function(res) {
+//             log(res)
+//             if (res.result === 0) {
+//                 let todayzj = res.infos.today_count
+//                 let t = `
+//                 <div class="number-2">${todayzj}</div>
+//                 `
+//                 su.html =  t
+//                 $('.secondline-all').html(su.html)
+//             }
+//         }
+//     })
+//     $.ajax(request)
+// }
+// people_count()
 
 // 金额，园区显示（本月数据）
 let month_xinxi = function () {
@@ -146,7 +179,7 @@ let month_xinxi = function () {
     $.ajax(request)
 }
 
-// 核销数目显示（今日数据）
+// 核销数目显示（本月数据）
 let month_shouru = function () {
     let request = ({
         url: "https://leyuanxing.net/newapi/Wxbossboard/incomeAndVisitor",
@@ -164,10 +197,11 @@ let month_shouru = function () {
             let money = res.info.month_income
             let people = res.info.month_visitors
             let order = res.info.month_orders
+            let monthzj = res.info.month_count
             log(money, people, order)
             let t = `
                 <div class="number-1">${order}</div>
-                <div class="number-2">人数</div>
+                <div class="number-2">${monthzj}</div>
             `
             su.html =  t
             $('.secondline-all').html(su.html)
